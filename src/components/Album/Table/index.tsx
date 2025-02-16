2fb9eff1e6cc58b7d9047dc2566c627ce8dd4a83
eu o erick <erickmesquita319@gmail.com>
2025-02-16 10:50:59 -0300

    add empty state for tracks not found

--- src/components/Album/Table/index.tsx
+++ src/components/Album/Table/index.tsx
@@ -1,6 +1,7 @@
 import { TTrack } from "@/types/TAlbum";
 import TrackListHeaderComponent from "./Header";
 import TableBodyComponent from "./Body";
+import { useTranslations } from "next-intl";
 
 export default function TrackTableComponent({
   tracks,
@@ -9,10 +10,12 @@ export default function TrackTableComponent({
   tracks: TTrack[] | undefined;
   isLoading: boolean;
 }) {
+  const t = useTranslations("AlbumPage");
+
   return (
     <div className="container py-10">
       {!isLoading && (!tracks || !tracks.length) ? (
-        <>num tem</>
+        <h4 className="text-zinc-500 text-center py-10">{t("noTracks")}</h4>
       ) : (
         <table className="w-full">
           <TrackListHeaderComponent />
